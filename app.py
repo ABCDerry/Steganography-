b64ce790acfed9bc17d48af5b6b9fc4eb2bc7ae9
ABCDerry <168010494+ABCDerry@users.noreply.github.com>
2025-04-05 14:26:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,6 @@ def index():
 
     return render_template("index.html", decrypted_message=decrypted_message)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # use Railway's PORT
+    app.run(host="0.0.0.0", port=port)
